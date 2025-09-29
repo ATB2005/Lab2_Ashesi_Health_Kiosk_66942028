0a4c949d0691d0ca3002685b2852a29b5342625c
ATB2005 <boatengakosua56@gmail.com>
2025-09-29 23:34:05 +0000

    final

--- src/HealthKiosk.java
+++ src/HealthKiosk.java
@@ -24,7 +24,7 @@ public class HealthKiosk {
                 System.out.println("Invalid service code");
         }
         byte healthMetric = 0;
-       double BMI = 0.0;
+        double BMI = 0.0;
         int numOfTablets = 0;
         double sinOfAngle = 0.0;
         if (serviceCode == 'T') {
@@ -37,7 +37,7 @@ public class HealthKiosk {
                     double weight = input.nextDouble();
                     System.out.print("Enter your height(m): ");
                     double height = input.nextDouble();
-                     BMI = weight / Math.pow(height, 2.0);
+                    BMI = weight / Math.pow(height, 2.0);
                     System.out.println("BMI:" + Math.round(BMI * 10) / 10.0);
                     if (weight >= 30) {
                         System.out.println("Category: Obese");
@@ -53,56 +53,74 @@ public class HealthKiosk {
                     System.out.print("Enter the required dosage(mg): ");
                     double dosage = input.nextDouble();
                     final int TABLETS = 250;
-                     numOfTablets = (int) Math.ceil(dosage) / TABLETS;
+                    numOfTablets = (int) Math.ceil(dosage) / TABLETS;
                     System.out.println("Number of tablets dispensed: " + numOfTablets);
                     break;
                 case 3:
                     System.out.print("Enter an angle in degrees: ");
                     double angle = input.nextDouble();
-                     sinOfAngle = Math.sin(angle);
+                    sinOfAngle = Math.sin(angle);
                     double cosOfAngle = Math.cos(angle);
                     System.out.println("The sin of the angle is " + (Math.round(sinOfAngle * 1000) / 1000.0) + " and the cos of the angle is " + (Math.round(cosOfAngle * 1000) / 1000.0));
                     break;
             }
         }
-        char randomChar = (char)('A'+(int)(Math.random()*26));
-        int randomNum1 = ( 3 + (int)(Math.random()*7));
-        int randomNum2 = ( 3 + (int)(Math.random()*7));
-        int randomNum3 = ( 3 + (int)(Math.random()*7));
-        int randomNum4 = ( 3 + (int)(Math.random()*7));
-        String ID = (""+ randomChar+randomNum1+randomNum2+randomNum3+randomNum4);
-        System.out.println("This is your new ID: " +ID);
+        char randomChar = (char) ('A' + (int) (Math.random() * 26));
+        int randomNum1 = (3 + (int) (Math.random() * 7));
+        int randomNum2 = (3 + (int) (Math.random() * 7));
+        int randomNum3 = (3 + (int) (Math.random() * 7));
+        int randomNum4 = (3 + (int) (Math.random() * 7));
+        String ID = ("" + randomChar + randomNum1 + randomNum2 + randomNum3 + randomNum4);
+        System.out.println("This is your new ID: " + ID);
         System.out.print("Enter your new ID: ");
         String shortID = input.next();
-        if (shortID.length()==5 && Character.isAlphabetic(shortID.charAt(0))&&(ID.charAt(0))==(shortID.charAt(0))){
-            if (Character.isDigit(shortID.charAt(1))&&Character.isDigit(shortID.charAt(2))&&Character.isDigit(shortID.charAt(3))&&Character.isDigit(shortID.charAt(4))) {
-                if((ID.charAt(1))==(shortID.charAt(1))&&(ID.charAt(2))==(shortID.charAt(2))&&(ID.charAt(3))==(shortID.charAt(3))&&(ID.charAt(4))==(shortID.charAt(4))){
+        if (shortID.length() == 5 && Character.isLetter(shortID.charAt(0)) && (ID.charAt(0)) == (shortID.charAt(0))) {
+            if (Character.isDigit(shortID.charAt(1)) && Character.isDigit(shortID.charAt(2)) && Character.isDigit(shortID.charAt(3)) && Character.isDigit(shortID.charAt(4))) {
+                if ((ID.charAt(1)) == (shortID.charAt(1)) && (ID.charAt(2)) == (shortID.charAt(2)) && (ID.charAt(3)) == (shortID.charAt(3)) && (ID.charAt(4)) == (shortID.charAt(4))) {
                     System.out.println("Valid ID");
                 }
             }
-        }
-        else if (shortID.length()!=5) {
+        } else if (shortID.length() != 5) {
             System.out.println("Invalid ID length");
-        }
-        else{
+        } else {
             System.out.println("Invalid: Last characters");
         }
         System.out.print("Enter your first name: ");
         String firstName = input.next();
         char baseCode = (firstName.toUpperCase().charAt(0));
         System.out.println("Base code = " + baseCode);
-        char shiftedLetter = (char) ('A'+(baseCode - 'A' + 2)%26);
+        char shiftedLetter = (char) ('A' + (baseCode - 'A' + 2) % 26);
         System.out.println("Shifted letter of base code = " + shiftedLetter);
-        String lastTwoChars = ID.substring(3,5);
+        String lastTwoChars = ID.substring(3, 5);
         System.out.println("Last two characters for ID: " + lastTwoChars);
-        if (healthMetric==1){
-            System.out.println("Code: "+ shiftedLetter+lastTwoChars+"-"+(int)BMI);
+        if (healthMetric == 1) {
+            System.out.println("Code: " + shiftedLetter + lastTwoChars + "-" + (int) BMI);
+        } else if (healthMetric == 2) {
+            System.out.println("Code: " + shiftedLetter + lastTwoChars + "-" + numOfTablets);
+        } else {
+            System.out.println("Code: " + shiftedLetter + lastTwoChars + "-" + (int) (sinOfAngle * 100));
         }
-        else if (healthMetric==2) {
-            System.out.println("Code: "+ shiftedLetter+lastTwoChars+"-"+numOfTablets);
-        }
-        else {
-            System.out.println("Code: "+ shiftedLetter+lastTwoChars+"-"+(int)(sinOfAngle *100));
+        switch (serviceCode) {
+            case 'P':
+                System.out.println("Summary: PHARMACY | ID:" + ID + "| Code = " + shiftedLetter + lastTwoChars  );
+                break;
+            case 'L':
+                System.out.println("Summary: LAB | ID: " + ID + "| Code = " + shiftedLetter + lastTwoChars );
+                break;
+            case 'T':
+                if (healthMetric == 1) {
+                    System.out.println("Summary: TRIAGE | ID:" + ID + "| Code = " + shiftedLetter + lastTwoChars + "-" + (int)BMI);
+                } else if (healthMetric == 2) {
+                    System.out.println("Summary: TRIAGE | ID:" + ID + "| Code = " + shiftedLetter + lastTwoChars + "-" + numOfTablets);
+                } else {
+                    System.out.println("Summary: TRIAGE | ID:" + ID + "| Code = " + shiftedLetter + lastTwoChars + "-" + (int) (sinOfAngle * 100));
+                }
+                break;
+            case 'C':
+                System.out.println("Summary: COUNSELLING | ID:" + ID + "| Code = " + shiftedLetter + lastTwoChars );
+                break;
+
         }
     }
 }
+
